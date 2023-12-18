3b56eed89c49892e9bde392d11ede9c3b76e4289
IvanOrsh <ksironov@mail.ru>
2023-12-18 11:56:36 +0300

    build: first commit

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,8 @@
+export default function NotesPage() {
+  return (
+    <section className="mt-10 px-4">
+      <h1 className="text-3xl font-bold underline text-center">Notes Page</h1>
+      <p className="text-base text-gray-600">Some content</p>
+    </section>
+  );
+}
